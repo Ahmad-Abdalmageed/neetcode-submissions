4df5a13dda8b28cd08eccca92e7d1f6310d2825f
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-07-02 16:17:12 +0300

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+
+        // O(n^2)
+        let res = 0;
+
+        // for(let i = 0; i < prices.length; i++) {
+        //     for(let j = i + 1; j < prices.length; j++) {
+        //         res = Math.max(res, prices[j] - prices[i]);
+        //     }
+        // }
+        // return res;
+
+        let i = 0;
+        let j = i + 1;
+
+        while(j < prices.length) {
+            if(prices[j] > prices[i]) {
+                res = Math.max(res, prices[j] - prices[i])
+            } else {
+                i = j
+            }
+            j++;
+            // while(i < j && prices[i] > prices[j]) i++;
+            // res = Math.max(res, prices[j] - prices[i]);
+            // j++;
+        }
+        return res;
+    }
+}
+ 
