7767e14f798b8c1f3089f27e9a96c9447f273119
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-07-14 11:13:10 +0300

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+
+        // Brute O(n*k)
+        // let res = [];
+        // let l = 0;
+        // let r = k - 1;
+
+        // while (r < nums.length) {
+        //     let max = -Infinity;
+        //     for (let i = l; i <= r; i++) {
+        //         max = Math.max(nums[i], max);
+        //     }
+        //     res.push(max);
+        //     r++;
+        //     l++;
+        // }
+        // return res;
+
+        const maxHeap = new MaxPriorityQueue((x) => x[0]);
+        let res = [];
+
+        let l = 0;
+        let r = k - 1;
+
+        for(let i = 0; i < k; i++) {
+            maxHeap.enqueue([nums[i], i]);
+
+        }
+
+        while(r < nums.length) {
+            maxHeap.enqueue([nums[r], r]);
+
+            while(maxHeap.front()[1] < l) {
+                maxHeap.dequeue();
+            }
+            res.push(maxHeap.front()[0]);
+            r++;
+            l++;
+        }
+        return res;        
+    }
+}
