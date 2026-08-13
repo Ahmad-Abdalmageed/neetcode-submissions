91c2ba0df0db92e52a62cbf0dc90a51ee65bb17c
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-08-13 16:41:05 +0300

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+
+    inorderTraversal(root) {
+        let ans = [];
+
+        function dfs(node) {
+            if (!node) return;
+            if (node.left) dfs(node.left);
+            ans.push(node.val);
+            if (node.right) dfs(node.right);
+        }
+
+        dfs(root);
+        return ans;
+    }
+}
