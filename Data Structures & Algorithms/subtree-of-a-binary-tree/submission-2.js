cceacdc141801618f8df5b8f35b8c61802c04de8
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-08-17 18:52:20 +0300

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+function sameTree(n1, n2) {
+    if (!n1 && !n2) return true;
+    if (!n1 || !n2 || n1.val !== n2.val) return false;
+    return sameTree(n1.left, n2.left) && sameTree(n1.right, n2.right);
+}
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        let ans = false;
+
+        function dfs(n1, n2) {
+            if (!n1) return;
+            if (n1.val === n2.val) {
+                if (sameTree(n1, n2)) {
+                    ans = true;
+                }
+            }
+            dfs(n1.left, n2);
+            dfs(n1.right, n2);
+        }
+        dfs(root, subRoot);
+        return ans;
+    }
+}
