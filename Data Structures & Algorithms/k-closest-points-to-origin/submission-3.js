f0dfcd97b33ba29d974dbf168ede046daf941895
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-08-29 12:41:44 +0300

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+
+        // Max Heap
+        // let heap = new MaxPriorityQueue((x) => x[0]);
+        // let ans = [];
+
+        // for (let i = 0; i < points.length; i++) {
+        //     let distance = Math.sqrt(Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2));
+        //     heap.enqueue([distance, points[i]]);
+
+        //     if (heap.size() > k) {
+        //         heap.dequeue();
+        //     }
+        // }
+
+        // while(heap.size()) {
+        //     ans.push(heap.front()[1]);
+        //     heap.dequeue();
+        // }
+        // return ans;
+
+        let heap = new MinPriorityQueue((x) => x[0]);
+        let ans = [];
+
+        for (let i = 0; i < points.length; i++) {
+            let distance = Math.sqrt(Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2));
+            heap.enqueue([distance, points[i]]);
+        }
+
+        for(let i = 0; i < k; i++) {
+            let point = heap.dequeue();
+            ans.push(point[1])
+        }
+
+        return ans;
+    }
+}
