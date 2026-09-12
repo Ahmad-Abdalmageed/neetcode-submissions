6d0c70f2c1e7e166b02de0c81c2bac02b7724180
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-09-12 11:33:16 +0300

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.js
@@ -0,0 +1,43 @@
+class MedianFinder {
+    constructor() {
+        this.right = new MinPriorityQueue();
+        this.left = new MaxPriorityQueue();
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    balance() {
+        let diff = this.right.size() - this.left.size();
+        console.log(diff);
+        if(diff < 0) {
+            this.right.enqueue(this.left.dequeue());
+        } else if(diff > 0) {
+            this.left.enqueue(this.right.dequeue());
+        } 
+    }
+
+    addNum(num) {
+        if(!this.left.size() || num >= this.left.front()) {
+            this.right.enqueue(num);
+        } else {
+            this.left.enqueue(num);
+        }
+        
+        this.balance();
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if(this.left.size() > this.right.size()) {
+            return this.left.front();
+        } else if(this.left.size() < this.right.size()) {
+            return this.right.front();
+        }
+        return (this.right.front() + this.left.front()) / 2
+    }
+}
