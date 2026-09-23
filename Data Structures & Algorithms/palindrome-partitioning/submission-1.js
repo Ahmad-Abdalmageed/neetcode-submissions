15a954ac8db52db01756c31933ec43344f3eb584
Ahmad Abdalmageed <ahmedmageed19@gmail.com>
2026-09-23 16:33:58 +0300

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string[][]}
+     */
+    partition(s) {
+        let ans = [];
+
+        function isPalindrome(left, right) {
+            while (left <= right) {
+                if (s[left] !== s[right]) return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        // function backtrack(path, start, idx) {
+        //     if (idx == s.length) {
+        //         if (start == s.length) {
+        //             ans.push([...path]);
+        //         }
+        //         return;
+        //     }
+
+        //     if (isPalindrome(start, idx)) {
+        //         path.push(s.slice(start, idx + 1));
+        //         backtrack(path, idx + 1, idx + 1);
+        //         path.pop();
+        //     }
+        //     backtrack(path, start, idx + 1);
+        // }
+
+        // backtrack([], 0, 0);
+
+        function backtrack(path, start) {
+            if(start == s.length) {
+                ans.push([...path]);
+                return;
+            }
+
+            for(let end = start; end < s.length; end++) {
+                if(!isPalindrome(start, end)) continue;
+                path.push(s.slice(start, end + 1));
+                backtrack(path, end + 1);
+                path.pop();
+            }
+        }
+        backtrack([], 0)
+        return ans;
+    }
+}
